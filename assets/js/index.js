2fbba5336a8eda672637612ebc86323bf141bf1f
Shimil Shaji Abraham <shimilsabraham@gmail.com>
2022-10-05 23:51:36 +0530

    Almost done !!
    Landing and navbar remains

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+// Swiper.js
+
+// const swiper = new Swiper(".swiper", {
+//     loop: true,
+//     speed: 1000,
+//     spaceBetween: 140,
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         dynamicBullets: true,
+//     },
+// });
