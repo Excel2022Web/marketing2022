7033050d3ba2452d249f164dd42e5920ddb3d32c
Shimil Shaji Abraham <shimilsabraham@gmail.com>
2022-10-08 21:39:13 +0530

    Changes Made !!

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,24 +22,24 @@ let toggle = 0;
 
 const menuIcon = document.querySelector(".icon");
 const menu = document.querySelector(".navbar .menu");
-menuIcon.addEventListener("click",()=>{
-  if(!toggle){
+menuIcon.addEventListener("click", () => {
+  if (!toggle) {
     menu.style.transform = "translate(-50%,0)";
-    toggle=1;
+    toggle = 1;
   }
-  else{
+  else {
     menu.style.transform = "translate(-50%,-120%)";
-    toggle=0;
+    toggle = 0;
   }
 });
 
-for(let i=0; i<menuItems.length; i++){
-  menuItems[i].addEventListener("click", ()=>{
-    if(toggle===1){
-     menu.style.transform = "translate(-50%,-100%)";
-    toggle=0;
+for (let i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener("click", () => {
+    if (toggle === 1) {
+      menu.style.transform = "translate(-50%,-100%)";
+      toggle = 0;
     }
-});
+  });
 };
 
 
@@ -48,20 +48,20 @@ const landingOptions = {
 };
 
 const landingObserver = new IntersectionObserver
-(function(
-  entries,
-  landingObserver
-){
-  entries.forEach(entry => {
-    if (!entry.isIntersecting){
-      navbar.classList.add("scrolled");
-    }
-    else{
-      navbar.classList.remove("scrolled");
-    }
-  });
-},
-landingOptions);
+  (function (
+    entries,
+    landingObserver
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        navbar.classList.add("scrolled");
+      }
+      else {
+        navbar.classList.remove("scrolled");
+      }
+    });
+  },
+    landingOptions);
 
 landingObserver.observe(landing);
 
@@ -73,130 +73,130 @@ const scrollOptions = {
 };
 
 const scrollObserver0 = new IntersectionObserver
-(function(entries, scrollObserver0){
-  entries.forEach(entry => {
-    if (entry.isIntersecting){
-      menuItems[current].classList.remove("active");
-      menuItems[0].classList.add("active");
-      current = 0;
-    }
-  });
-},
-scrollOptions);
+  (function (entries, scrollObserver0) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        menuItems[current].classList.remove("active");
+        menuItems[0].classList.add("active");
+        current = 0;
+      }
+    });
+  },
+    scrollOptions);
 
 scrollObserver0.observe(sections[0]);
 
 
 const scrollObserver1 = new IntersectionObserver
-(function(entries, scrollObserver1){
-  entries.forEach(entry => {
-    if (entry.isIntersecting){
-      menuItems[current].classList.remove("active");
-      menuItems[1].classList.add("active");
-      current = 1;
-    }
-  });
-},
-scrollOptions);
+  (function (entries, scrollObserver1) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        menuItems[current].classList.remove("active");
+        menuItems[1].classList.add("active");
+        current = 1;
+      }
+    });
+  },
+    scrollOptions);
 
 scrollObserver1.observe(sections[1]);
 
 
 const scrollObserver2 = new IntersectionObserver
-(function(entries, scrollObserver2){
-  entries.forEach(entry => {
-    if (entry.isIntersecting){
-      menuItems[current].classList.remove("active");
-      menuItems[2].classList.add("active");
-      current = 2;
-    }
-  });
-},
-scrollOptions);
+  (function (entries, scrollObserver2) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        menuItems[current].classList.remove("active");
+        menuItems[2].classList.add("active");
+        current = 2;
+      }
+    });
+  },
+    scrollOptions);
 
 scrollObserver2.observe(sections[2]);
 
 const scrollObserver3 = new IntersectionObserver
-(function(entries, scrollObserver3){
-  entries.forEach(entry => {
-    if (entry.isIntersecting){
-      menuItems[current].classList.remove("active");
-      menuItems[3].classList.add("active");
-      current = 3;
-    }
-  });
-},
-scrollOptions);
+  (function (entries, scrollObserver3) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        menuItems[current].classList.remove("active");
+        menuItems[3].classList.add("active");
+        current = 3;
+      }
+    });
+  },
+    scrollOptions);
 
 scrollObserver3.observe(sections[3]);
 
 
 const scrollObserver4 = new IntersectionObserver
-(function(entries, scrollObserver4){
-  entries.forEach(entry => {
-    if (entry.isIntersecting){
-      menuItems[current].classList.remove("active");
-      menuItems[4].classList.add("active");
-      current = 4;
-    }
-  });
-},
-scrollOptions);
+  (function (entries, scrollObserver4) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        menuItems[current].classList.remove("active");
+        menuItems[4].classList.add("active");
+        current = 4;
+      }
+    });
+  },
+    scrollOptions);
 
 scrollObserver4.observe(sections[4]);
 
 
 const scrollObserver5 = new IntersectionObserver
-(function(entries, scrollObserver5){
-  entries.forEach(entry => {
-    if (entry.isIntersecting){
-      menuItems[current].classList.remove("active");
-      menuItems[5].classList.add("active");
-      current = 5;
-    }
-  });
-},
-scrollOptions);
+  (function (entries, scrollObserver5) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        menuItems[current].classList.remove("active");
+        menuItems[5].classList.add("active");
+        current = 5;
+      }
+    });
+  },
+    scrollOptions);
 
 scrollObserver5.observe(sections[5]);
 
 const scrollObserver6 = new IntersectionObserver
-(function(entries, scrollObserver6){
-  entries.forEach(entry => {
-    if (entry.isIntersecting){
-      menuItems[current].classList.remove("active");
-      menuItems[6].classList.add("active");
-      current = 6;
-    }
-  });
-},
-scrollOptions);
+  (function (entries, scrollObserver6) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        menuItems[current].classList.remove("active");
+        menuItems[6].classList.add("active");
+        current = 6;
+      }
+    });
+  },
+    scrollOptions);
 
 scrollObserver6.observe(sections[6]);
 
 
 const scrollObserver7 = new IntersectionObserver
-(function(entries, scrollObserver7){
-  entries.forEach(entry => {
-    if (entry.isIntersecting){
-      menuItems[current].classList.remove("active");
-      menuItems[7].classList.add("active");
-      current = 7;
-    }
-  });
-},
-scrollOptions);
+  (function (entries, scrollObserver7) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        menuItems[current].classList.remove("active");
+        menuItems[7].classList.add("active");
+        current = 7;
+      }
+    });
+  },
+    scrollOptions);
 
 scrollObserver7.observe(sections[7]);
 
 // active on click
 
-for(let i=0; i<menuItems.length; i++){
-  menuItems[i].addEventListener("click", ()=>{
-    if(i!==current){
+for (let i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener("click", () => {
+    if (i !== current) {
       menuItems[current].classList.remove("active");
     }
     sections[i].scrollIntoView();
     current = i;
-});
+  });
 };
